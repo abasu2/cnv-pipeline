a2286f948cda7ba9f2af9ba47e5c71fcc540bed6
abasu2 <anirudhbasu1@gmail.com>
2024-05-26 19:01:43 -0700

    Removed incorrect python call

--- single.sh
+++ single.sh
@@ -3,4 +3,4 @@
 sample_name=$1
 sample_path="/share/lanzarolab/seq/map/AgamP4/*samples/$sample_name/$sample_name.bam"
 fasta_file="/share/lanzarolab/users/abasu/reference/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP4.fa"
-python ~/cnv_data_fixed.py $sample_name $fasta_file $sample_path /home/abasu/accessibility_masks /home/abasu/cnv_results/$sample_name
+python cnv_data_fixed.py $sample_name $fasta_file $sample_path /home/abasu/accessibility_masks /home/abasu/cnv_results/$sample_name
